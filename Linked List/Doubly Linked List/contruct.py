6cc52bf9b48c0e98a95867faad0c051a45a64ff1
Pk Patel <parthpatel9414@gmail.com>
2024-03-01 03:54:39 +0000

    Add Doubly Linked List implementation

---
+++ Linked List/Doubly Linked List/contruct.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data = None, next = None, prev = None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail - new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
